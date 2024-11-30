f51b93e3cd8cb66a4903f485d47481c683684fe1
Clément Chanchevrer <clement.chanchevrier@gmail.com>
2024-11-30 17:56:52 +0100

    fix(server): type errors

--- packages/server/src/routes/api.ts
+++ packages/server/src/routes/api.ts
@@ -9,7 +9,7 @@ router.get("/todos", async (req, res) => {
     const todos = await Todo.findAll();
     res.json(todos);
   } catch (error) {
-    res.status(500).send(error.message);
+    res.status(500).send((error as Error).message);
   }
 });
 
@@ -20,7 +20,7 @@ router.post("/todos", async (req, res) => {
     await newTodo.save();
     res.sendStatus(201);
   } catch (error) {
-    res.status(500).send(error.message);
+    res.status(500).send((error as Error).message);
   }
 });
 
@@ -28,10 +28,12 @@ router.delete("/todos/:id", async (req, res) => {
   try {
     const { id } = req.params;
     const todo = await Todo.findByPk(id);
-    await todo.destroy();
+    if (todo) {
+      await todo.destroy();
+    }
     res.sendStatus(200);
   } catch (error) {
-    res.status(500).send(error.message);
+    res.status(500).send((error as Error).message);
   }
 });
 
@@ -40,10 +42,15 @@ router.patch("/todos/:id", async (req, res) => {
     const { id } = req.params;
     const { completed } = req.body;
     const todo = await Todo.findByPk(id);
+
+    if (!todo) {
+      throw new Error("Could not find the todo to update");
+    }
+
     await todo.update({ completed });
     res.sendStatus(201);
   } catch (error) {
-    res.status(500).send(error.message);
+    res.status(500).send((error as Error).message);
   }
 });
 
